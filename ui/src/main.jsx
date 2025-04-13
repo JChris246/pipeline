1025dc0ec6edce36cda4e0941cfe9dafc595a9db
JChris246 <JChris246@users.noreply.github.com>
2025-04-12 23:55:14 -0400

    chore: rearrange and add some utils

--- ui/src/main.jsx
+++ ui/src/main.jsx
@@ -1,10 +1,29 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+import App from "./pages/App";
+import Error from "./pages/Error";
 import "./index.css";
-import App from "./App.jsx";
+
+import { AppProvider } from "./AppContext";
+import { Notification, NotificationProvider } from "./components/Notification";
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <App />,
+        errorElement: <Error />,
+    }
+]);
 
 createRoot(document.getElementById("root")).render(
     <StrictMode>
-        <App />
-    </StrictMode>,
+        <NotificationProvider>
+            <Notification />
+            <AppProvider>
+                <RouterProvider router={router} />
+            </AppProvider>
+        </NotificationProvider>
+    </StrictMode>
 );
